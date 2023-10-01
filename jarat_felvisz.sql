d8337bc371010e11620aebe9f127f69352e0b76e
vbalazs01 <146450627+vbalazs01@users.noreply.github.com>
2023-10-01 17:15:05 +0200

    Insert into a Jarat táblához

---
+++ jarat_felvisz.sql
@@ -0,0 +1,2 @@
+insert into Jarat (Jaratszam, Megnevezes, Honnan, Hova, Indulas, Erkezes, Tavolsag, Menetido, Ar, Helyjegy, VonalUzemelteto, VonatTipus)
+values (31916, 'személyvonat', 'Pápa', 'Csorna', '17:39', '18:42',37, CONCAT(FLOOR(DATEDIFF(second, '17:39', '18:42') / 3600), ' óra ', FLOOR((DATEDIFF(second, '17:39', '18:42') % 3600) / 60), ' perc '), 560, 0, 2, 'M46')
